72962d0912c62bff79fb44fa42733ebf87ec899c
Pierre <49315644+ech1@users.noreply.github.com>
2021-01-28 14:21:58 +0100

    added files

---
+++ swamp.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+
+GOTO :A
+
+:A
+echo [+] Swamp Automation Script
+echo 1) Active
+echo 2) Idle
+echo 3) Textmode
+
+set /p CHOICE=[+] Type the number of choice: 
+echo %CHOICE%
+
+IF "%CHOICE%"=="1" echo [+] ACTIVE MODE SELECTED   & PowerShell.exe -Command ".\swamp_act.ps1" & PAUSE
+IF "%CHOICE%"=="2" echo [+] IDLE MODE SELECTED     & PowerShell.exe -Command ".\swamp_idl.ps1" & PAUSE
+IF "%CHOICE%"=="3" echo [+] TEXTMODE SELECTED      & PowerShell.exe -Command ".\swamp_textmode.ps1" & PAUSE
+timeout 60
+
+GOTO :A
