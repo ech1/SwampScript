febfac15d725d80e64f8cda6046cb78783a50506
Pierre <49315644+ech1@users.noreply.github.com>
2021-01-29 21:54:11 +0100

    update

--- swamp.bat
+++ swamp.bat
@@ -12,9 +12,9 @@ echo 3) Textmode
 set /p CHOICE=[+] Type the number of choice: 
 echo %CHOICE%
 
-IF "%CHOICE%"=="1" echo [+] ACTIVE MODE SELECTED   & PowerShell.exe -Command ".\swamp_act.ps1" & PAUSE
-IF "%CHOICE%"=="2" echo [+] IDLE MODE SELECTED     & PowerShell.exe -Command ".\swamp_idl.ps1" & PAUSE
-IF "%CHOICE%"=="3" echo [+] TEXTMODE SELECTED      & PowerShell.exe -Command ".\swamp_textmode.ps1" & PAUSE
+IF "%CHOICE%"=="1" echo [+] ACTIVE MODE SELECTED   & PowerShell.exe -Command ".\swamp_act.ps1" & GOTO :A
+IF "%CHOICE%"=="2" echo [+] IDLE MODE SELECTED     & PowerShell.exe -Command ".\swamp_idl.ps1" & GOTO :A
+IF "%CHOICE%"=="3" echo [+] TEXTMODE SELECTED      & PowerShell.exe -Command ".\swamp_textmode.ps1" & GOTO :A
 timeout 60
 
 GOTO :A
